cf93605453a25e7656d2e7fb5c3bfc8d8035b0a6
Dave Edelhart <dave@wonderlandlabs.com>
2014-05-06 09:45:33 -0700

    speeding up bots with web workers

--- test_site/bots-worker.js
+++ test_site/bots-worker.js
@@ -1,30 +1,6 @@
-var botsWorker = new Worker('bots-worker.js');
-
-var addDomRE = /^addDom (.*)/;
-var moveRE = /^move ([\d]+) (.*)/;
-botsWorker.onmessage = function(msg){
-    console.log(msg);
-    var d = msg.data;
-    if (addDomRE.test(d)){
-        document.body.insertAdjacentHTML('afterend', addDomRE.exec(d)[1]);
-    } else if (moveRE.test(d)){
-        var match = moveRE.exec(d);
-        var mid = parseInt(match[1]);
-        var loc = JSON.parse(match[2]);
-        moveMember(mid, loc);
-    }
-};
-
-function moveMember(mid, loc) {
-    var ele = document.getElementById('member-' + mid);
-    ele.style.left = (loc.i * 25 + loc.k * 3) + 'px';
-    ele.style.top = (loc.j * 25 + loc.k * 3) + 'px';
-    ele.style.zIndex = loc.k;
-    var v = ((loc.k  * 10) % 255);
-    ele.style.backgroundColor = 'rgb(' + loc.k + ',' + v + ',' + v + ')';
-}
-
-/*
+importScripts('lodash.min.js');
+importScripts('fools.js');
+importScripts('n-space.js');
 
 var world = new NSPACE.World({
     i: [0, 60],
@@ -32,7 +8,7 @@ var world = new NSPACE.World({
     k: [0, 60]
 });
 
-var _div = _.template('<div class="member" id="member-<%=member.mid %>"><%= member.mid %></div>');
+var _div = _.template('<div class="member" id="member-<%=member.mid %>"><%= member.mid %></div>')
 
 function makeBot(loc) {
     var bot = new NSPACE.WanderBot('bot', world, loc);
@@ -45,13 +21,8 @@ function makeBot(loc) {
         NSPACE.WanderBotScanRule.checkStack(bot, NSPACE.WanderBotScanRule.forward('k', bot))); // move on J -- forward preferred
 
     function moveMember(member) {
-        var loc = member.loc;
-        var ele = document.getElementById('member-' + member.mid);
-        ele.style.left = (loc.i * 25 + loc.k * 3) + 'px';
-        ele.style.top = (loc.j * 25 + loc.k * 3) + 'px';
-        ele.style.zIndex = loc.k;
-        var v = ((loc.k  * 10) % 255);
-        ele.style.backgroundColor = 'rgb(' + loc.k + ',' + v + ',' + v + ')';
+        var msg = 'move ' + member.mid + ' ' + JSON.stringify(member.loc);
+        postMessage(msg);
     }
 
     function slideMember(member) {
@@ -85,9 +56,9 @@ function makeBot(loc) {
         bot.move();
     }, 200);
 
-    document.body.insertAdjacentHTML('afterend', _div(bot));
+    postMessage('addDom ' + _div(bot));
 
     return bot;
 }
 
-makeBot({i: 0, j: 0, k: 0});*/
+makeBot({i: 0, j: 0, k: 0});
