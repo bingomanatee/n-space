c716ef07e1e8348268e7a93650dbcafd58e4bd7c
Dave Edelhart <dave@wonderlandlabs.com>
2014-05-10 13:23:43 -0700

    work on range slider

--- tiles/app/src/Settings.js
+++ tiles/app/src/Settings.js
@@ -10,6 +10,15 @@ define(function (require, exports, module) {
     var Transform = require('famous/core/Transform');
     var buttonClasses = require('./buttonClasses');
     var buttonMenu = require('./buttonMenu');
+    var InputSurface = require('famous/surfaces/InputSurface');
+
+    // size constants
+    var RANGE_SLIDER_HEIGHT = 16;
+    var RANGE_SLIDER_WIDTH = 130;
+
+    var RANGE_MIN_SIZE = 10;
+    var RANGE_SLIDER_LABEL_WIDTH = 30;
+    var RANGE_MAX_SIZE = 100;
 
     function Settings(options, buttons) {
         View.call(this, _.defaults({}, options, Settings.DEFAULTS));
@@ -35,37 +44,8 @@ define(function (require, exports, module) {
 
         this._node.add(this.back);
 
-        var hexButton = new ToggleButton({
-            content: 'Hexagons',
-            classes: ['button'],
-            size: [true, this.options.buttonHeight]
-        });
-        hexButton.deselect();
-
-        buttonClasses(hexButton, ['mode-button']);
-
-        var squareButton = new ToggleButton({
-            content: 'Squares',
-            classes: ['button'],
-            size: [true, this.options.buttonHeight]
-        });
-        squareButton.select();
-
-        buttonClasses(squareButton, ['mode-button']);
-
-        this.modeChoiceRenderNode = this.back.add(new Modifier({
-            transform: Transform.translate(
-                this.options.margin,
-                this.options.headingSize, 0)
-        }));
-        this.modeChoiceRenderNode.add(hexButton);
-
-        this.modeChoiceRenderNode.add(new Modifier({
-            transform: Transform.translate(0, this.options.buttonHeight * 1.5, 0)
-        })).add(squareButton);
-
-        buttonMenu([squareButton, hexButton])
-
+        this._initGridButtons();
+        this._initRangeSlider();
     }
 
     module.exports = Settings;
@@ -88,5 +68,128 @@ define(function (require, exports, module) {
 
     _.extend(Settings.prototype, {
 
-    });
-});
+        _rangeTitle: function(){
+            return  'Tile Size: ' + this.gridSize;
+        },
+
+        _initRangeSlider: function () {
+
+            /* ------------ range slider ---------------- */
+
+            this.gridSize = Math.round(( RANGE_MIN_SIZE + RANGE_MAX_SIZE) / 2);
+
+            var rangeSlider = new InputSurface({
+                'type': 'range',
+                min: RANGE_MIN_SIZE,
+                max: RANGE_MAX_SIZE,
+                size: [RANGE_SLIDER_WIDTH, RANGE_SLIDER_HEIGHT],
+                value: this.gridSize
+            });
+
+            rangeSlider.on('change', function(value){
+                var n = parseInt(value.target.value);
+                this.gridSize = n;
+                this._eventInput.emit('grid size', n);
+                console.log('range value: ', n);
+
+                title.setContent(this._rangeTitle());
+            }.bind(this));
+
+            var sliderRo = this.back.add(new Modifier({
+                size: [150, 30],
+                origin: [0, 0],
+                transform: Transform.translate(170, this.options.headingSize * 2)
+            }));
+
+            /* --------------- range title ------------------ */
+
+            var title = new Surface({
+                content: this._rangeTitle(),
+                size: [RANGE_SLIDER_WIDTH, true]
+            });
+
+            title.elementType = 'h3';
+
+            sliderRo.add(new Modifier({
+                transform: Transform.translate(0, -RANGE_SLIDER_HEIGHT),
+                origin: [0, 0]
+            })).add(title);
+            sliderRo.add(rangeSlider);
+
+            /* ---------------- min label ----------------- */
+
+            var minLabel = new Surface({
+                size: [RANGE_SLIDER_LABEL_WIDTH, RANGE_SLIDER_HEIGHT],
+                content: '' + RANGE_MIN_SIZE,
+                properties: {
+                    textAlign: 'right',
+                    paddingRight: '2px'
+                }
+            });
+
+            minLabel.elementType = 'label';
+
+            sliderRo.add(new Modifier({
+                transform: Transform.translate(-RANGE_SLIDER_LABEL_WIDTH, 0, 0)
+            })).add(minLabel);
+
+            /* ------------- max label ------------------- */
+
+            var maxLabel = new Surface({
+                size: [RANGE_SLIDER_LABEL_WIDTH, RANGE_SLIDER_HEIGHT],
+                content: '' + RANGE_MAX_SIZE,
+                properties: {
+                    textAlign: 'right',
+                    paddingRight: '2px'
+                }
+            });
+
+            maxLabel.elementType = 'label';
+
+            sliderRo.add(new Modifier({
+                transform: Transform.translate(RANGE_SLIDER_WIDTH, 0, 0)
+            })).add(maxLabel);
+        },
+
+        _initGridButtons: function () {
+
+            var hexButton = new ToggleButton({
+                content: 'Hexagons',
+                classes: ['button'],
+                size: [true, this.options.buttonHeight]
+            });
+            hexButton.deselect();
+
+            buttonClasses(hexButton, ['mode-button']);
+
+            var squareButton = new ToggleButton({
+                content: 'Squares',
+                classes: ['button'],
+                size: [true, this.options.buttonHeight]
+            });
+
+            buttonClasses(squareButton, ['mode-button']);
+
+            this.modeChoiceRenderNode = this.back.add(new Modifier({
+                transform: Transform.translate(
+                    this.options.margin,
+                    this.options.headingSize, 0)
+            }));
+            this.modeChoiceRenderNode.add(hexButton);
+
+            this.modeChoiceRenderNode.add(new Modifier({
+                transform: Transform.translate(0, this.options.buttonHeight * 1.5, 0)
+            })).add(squareButton);
+
+            buttonMenu([hexButton, squareButton], this.onModeButton.bind(this));
+            squareButton.select();
+            this.gridMode = 1;
+        },
+
+        onModeButton: function (button, mode) {
+            console.log('grid mode: ', mode);
+        }
+    })
+    ;
+})
+;
